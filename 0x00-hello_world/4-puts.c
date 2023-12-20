15530f0b53dc9ac70829886e5c0e6faa0a52bb73
vuyo mbamba <vuyombamba13@gmail.com>
2023-12-20 13:39:41 -0800

    updated'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(str );
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
