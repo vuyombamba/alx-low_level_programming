8c8e1bf6675fbfa7ec53705355993410d850f563
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-04 12:42:29 -0800

    added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+	int i = 0, j;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			if (j * i > 9)
+			{
+				_putchar((j * i) / 10 + '0');
+				_putchar((j * i) % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((j * i) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
