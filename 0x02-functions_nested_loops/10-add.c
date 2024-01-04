700b78b3e242057c45563514ea86ae9c8a6b0c70
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-04 13:11:36 -0800

    fixed error on 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
+/**
+ * add - adds two numbers
+ * @i: parameter intakes a int value
+ * @j: parameter that intake int values
+ * Return: Int value
+ */
 int add(int i, int j)
 {
-	return (i+j);
+	return (i + j);
 }
