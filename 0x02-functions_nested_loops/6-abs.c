2ec17f0c2e2966909e4c9cb70d83b9394fd84b3a
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-04 06:45:44 -0800

    added 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - it calculates the absolute value of the parameter
+ * @i: parameter to intake a value
+ * Return: Always the size of int
+ */
+int _abs(int i)
+{
+	return (i < 0 ? -i : i);
+}
