1948ecd35aea3f7ab5c3bdd33f6694c0299da631
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-04 11:38:17 -0800

    Added 8-25_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 24)
+	{
+		int j = 0;
+
+		while (j < 61)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
