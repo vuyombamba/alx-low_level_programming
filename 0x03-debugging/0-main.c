53daf766ca014e5fcbdb8f76aa79be52ff67f456
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-08 07:11:58 -0800

    Error fix on 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - main function
+ * main - tests function that prints if integer is positive or negative\
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int i = 98;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
