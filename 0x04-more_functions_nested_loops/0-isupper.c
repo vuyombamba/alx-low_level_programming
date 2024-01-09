d27b06971f335dd25df02d486dbc08f0ccfa1215
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-09 08:17:31 -0800

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _isupper - checks if the parameter is an uppercase
+ * @c: int value of a char
+ * Return: 1 (success) or 0 (fail)
+ */
 int _isupper(int c)
 {
 	if (c > 40 && c < 92)
