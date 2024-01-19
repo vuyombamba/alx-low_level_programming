0e7db1390f17ad529115e5321c3d34cf27ad5271
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-19 14:58:56 -0800

    added 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: the length of the line
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		int j = 1;
+
+		while (j <= i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
