bbb0587d9d1786d3b732d96dd326e9f42a8642be
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-23 09:56:24 -0800

    update 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz ");
 		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s", "Fizz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("%s", "Buzz ");
 		}
 		else
 		{
