82ef6a95f794d8256d1bcf7b644ffc5010e2ec1e
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-25 08:15:12 -0800

    added 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: String to test
+ * Return: int legth of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0x05. C - pointers, arrays and strings
 0. program that updates a value using pointers
 1. Swaps two values using pointers
+2. Checks the length of the string
