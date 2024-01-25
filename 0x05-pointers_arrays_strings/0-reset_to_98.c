68ff29706641fe03aaa5f048fa07a3ff510d62e8
vuyo mbamba <vuyombamba13@gmail.com>
2024-01-25 07:01:05 -0800

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the parameter to 98
+ * @*n: value address
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x05. C - pointers, arrays and strings
+0. program that updates a value using pointers
