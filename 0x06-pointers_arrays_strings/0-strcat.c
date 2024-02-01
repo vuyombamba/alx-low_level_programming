f5968a26498a733fa75484d96114ebe0250dae5b
vuyo mbamba <vuyombamba13@gmail.com>
2024-02-01 03:21:20 -0800

    added 0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - the function appends a string to another string
+ * @dest: string variable to append to
+ * @src: the copied string
+ * Return: the copied string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while(*(src + i) != '\n')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
