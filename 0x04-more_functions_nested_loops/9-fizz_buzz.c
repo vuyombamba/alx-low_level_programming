afe9b2f427baed9793a5c4920deb32aa545b3c71
vuyo mbamba <vuyombamba@icloud.com>
2024-03-25 20:08:58 +0000

    updated error on fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,23 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("%s", "FizzBuzz ");
 		}
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("%s", "Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
-			printf("%s", "Buzz ");
+			if (i != 100 )
+			{
+				printf("%s", "Buzz ");
+			}else
+			{
+				printf("%s", "Buzz");
+			}
 		}
 		else
 		{
